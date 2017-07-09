18207ee7b880b7129ba093817bf4dbd891a6e3be
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2017-07-09 21:35:25 +0530

    Fixing issue where 'redirect_to :back' is no longer supported in Rails 5.

--- lib/active_admin_paranoia/dsl.rb
+++ lib/active_admin_paranoia/dsl.rb
@@ -9,12 +9,24 @@ module ActiveAdminParanoia
 
       batch_action :destroy, confirm: proc{ I18n.t('active_admin.batch_actions.delete_confirmation', plural_model: resource_class.to_s.downcase.pluralize) }, if: proc{ authorized?(ActiveAdmin::Auth::DESTROY, resource_class) && params[:scope] != 'archived' } do |ids|
         resource_class.to_s.camelize.constantize.where(id: ids).destroy_all
-        redirect_to :back, notice: I18n.t('active_admin.batch_actions.succesfully_destroyed', count: ids.count, model: resource_class.to_s.camelize.constantize.model_name, plural_model: resource_class.to_s.downcase.pluralize)
+        options = { notice: I18n.t('active_admin.batch_actions.succesfully_destroyed', count: ids.count, model: resource_class.to_s.camelize.constantize.model_name, plural_model: resource_class.to_s.downcase.pluralize) }
+        # For more info, see here: https://github.com/rails/rails/pull/22506
+        if Rails::VERSION::MAJOR >= 5
+          redirect_back({ fallback_location: ActiveAdmin.application.root_to }.merge(options))
+        else
+          redirect_to :back, options
+        end
       end
 
       batch_action :restore, confirm: proc{ I18n.t('active_admin_paranoia.batch_actions.restore_confirmation', plural_model: resource_class.to_s.downcase.pluralize) }, if: proc{ authorized?(ActiveAdminParanoia::Auth::RESTORE, resource_class) && params[:scope] == 'archived' } do |ids|
         resource_class.to_s.camelize.constantize.restore(ids, recursive: true)
-        redirect_to :back, notice: I18n.t('active_admin_paranoia.batch_actions.succesfully_restored', count: ids.count, model: resource_class.to_s.camelize.constantize.model_name, plural_model: resource_class.to_s.downcase.pluralize)
+        options = { notice: I18n.t('active_admin_paranoia.batch_actions.succesfully_restored', count: ids.count, model: resource_class.to_s.camelize.constantize.model_name, plural_model: resource_class.to_s.downcase.pluralize) }
+        # For more info, see here: https://github.com/rails/rails/pull/22506
+        if Rails::VERSION::MAJOR >= 5
+          redirect_back({ fallback_location: ActiveAdmin.application.root_to }.merge(options))
+        else
+          redirect_to :back, options
+        end
       end
 
       action_item :restore, only: :show do
@@ -23,7 +35,13 @@ module ActiveAdminParanoia
 
       member_action :restore, method: :put, confirm: proc{ I18n.t('active_admin_paranoia.restore_confirmation') }, if: proc{ authorized?(ActiveAdminParanoia::Auth::RESTORE, resource_class) } do
         resource.restore(recursive: true)
-        redirect_to :back, notice: I18n.t('active_admin_paranoia.batch_actions.succesfully_restored', count: 1, model: resource_class.to_s.camelize.constantize.model_name, plural_model: resource_class.to_s.downcase.pluralize)
+        options = { notice: I18n.t('active_admin_paranoia.batch_actions.succesfully_restored', count: 1, model: resource_class.to_s.camelize.constantize.model_name, plural_model: resource_class.to_s.downcase.pluralize) }
+        # For more info, see here: https://github.com/rails/rails/pull/22506
+        if Rails::VERSION::MAJOR >= 5
+          redirect_back({ fallback_location: ActiveAdmin.application.root_to }.merge(options))
+        else
+          redirect_to :back, options
+        end
       end
 
       scope(I18n.t('active_admin_paranoia.non_archived'), default: true) { |scope| scope.where(resource_class.to_s.camelize.constantize.paranoia_column => resource_class.to_s.camelize.constantize.paranoia_sentinel_value) }
